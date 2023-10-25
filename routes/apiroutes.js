437db523f4d9195c437e4a26f8c0a1f6cc56876f
JW <daxon2012@gmail.com>
2023-10-24 20:23:32 -0700

    Adding and importing files to apirouters.js

---
+++ routes/apiroutes.js
@@ -0,0 +1,9 @@
+// The `Router` function is from the `express` module.
+    // This module could create a new router object which allows me to define routes for the application
+// I import the built-in Node.js fs (File System) module
+    // This module provides methods for working with the file system
+// I import the 'uniqid' module
+    // This module is used to generate unique IDs.
+const router = require("express").Router()
+const fs = require("fs");
+const uniqid = require("uniqid");
